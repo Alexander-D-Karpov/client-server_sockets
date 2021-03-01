1512ecdcbfbb63976eb004fdeaf6cea0c3419525
Alexandr Karpov <51019345+alexandrportx@users.noreply.github.com>
2021-03-01 23:47:46 +0300

    created simple server

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+sock = socket.socket()
+sock.bind(('', 8888))
+sock.listen(1)
+conn, adr = sock.accept()
+
+print('Look at this boy, who connected: ' + str(adr))
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.send(data.upper)
+
+conn.close
