0c5a94b9e2fc742713cf0e1f5e91c34dba6f7384
Alexandr Karpov <51019345+alexandrportx@users.noreply.github.com>
2021-03-03 23:56:05 +0300

    added stream with cv

--- client.py
+++ client.py
@@ -1,40 +1,22 @@
-with open('ip.env', mode='r') as file:
-    server_ip = file.read()
-from socket import socket
-from tkinter import *
+import cv2
+import numpy as np
+import socket
+import sys
+import pickle
+import struct
+import pyautogui
+import time
+from PIL import Image
 
-def login():
-    logn = str(log.get()) + str(pas.get())
-    sock = socket.socket()
-    sock.connect((server_ip, 4040))
-    sock.send(logn.encode())
-    data = sock.recv(1024).decode()
-    sock.close()
-    if data == 'True':
-            lbl.configure(text = 'Ur logged in')
-            msg = ("Hello you there!").encode('utf-8')
-            log.destroy()
-            lbl1.destroy()
-            pas.destroy()
-            btn.destroy()
-
-
-window = Tk()
-window.geometry('400x250')
-window.title("Client")
-lbl = Label(window, text="Login")
-lbl.grid(column=0, row=0)
-log = Entry(window,width=10)  
-log.grid(column=1, row=0)
-
-lbl1 = Label(window, text="Password")
-lbl1.grid(column=0, row=1)
-pas = Entry(window,width=10)  
-pas.grid(column=1, row=1)
-
-btn = Button(window, text="Log in", command=login)  
-btn.grid(column=0, row=3)  
-window.mainloop()
-
-
-
+ 
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(('localhost', 7777))
+ 
+ 
+while True:
+    image = pyautogui.screenshot()
+    image = image.resize((192, 108), Image.ANTIALIAS)
+    image = np.array(image)
+    img = Image.frombytes('RGB', (192, 108), image)
+    data = pickle.dumps(np.array(img))
+    clientsocket.sendall(struct.pack("L", len(data)) + data)
