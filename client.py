8bc0307cace199ff8b4269d3285b6f9c3e6c0aad
Alexandr Karpov <alexandr.d.karpov@gmail.com>
2021-03-04 11:30:05 +0300

    trying to create streaming

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 with open('ip.env', mode='r') as file:
     server_ip = file.read()
-from socket import socket
+import socket
 from tkinter import *
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct
+from PIL import Image
 
 def login():
     logn = str(log.get()) + str(pas.get())
@@ -12,11 +18,51 @@ def login():
     sock.close()
     if data == 'True':
             lbl.configure(text = 'Ur logged in')
-            msg = ("Hello you there!").encode('utf-8')
             log.destroy()
             lbl1.destroy()
             pas.destroy()
-            btn.destroy()
+            btn.configure(text = 'Start streaming', command=start_stream)
+
+def start_stream():
+    sock = socket.socket()
+    sock.connect((server_ip, 4040))
+    check_data = 'True'
+    sock.send(check_data.encode())
+    sock.close()
+    HOST = 'localhost'
+    PORT = 7777
+    
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+    
+    s.bind((HOST, PORT))
+    print('Socket bind complete')
+    s.listen(10)
+    print('Socket now listening')
+    
+    conn, addr = s.accept()
+    
+    data = b''
+    payload_size = struct.calcsize("L")
+    
+    while True:
+        while len(data) < payload_size:
+            data += conn.recv(4096)
+        packed_msg_size = data[:payload_size]
+    
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+    
+        while len(data) < msg_size:
+            data += conn.recv(4096)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+    
+        frame=pickle.loads(frame_data)
+        print(frame.size)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        cv2.imshow('Заголовок окна', frame)
+        cv2.waitKey(10)
 
 
 window = Tk()
--- server.py
+++ server.py
@@ -1,4 +1,12 @@
+import cv2
+import numpy as np
 import socket
+import sys
+import pickle
+import struct
+import pyautogui
+import time
+from PIL import Image
 
 sock = socket.socket()
 sock.bind(('', 4040))
@@ -8,10 +16,23 @@ while True:
     conn, addr = sock.accept()
     print('connected:', addr)
     data = conn.recv(1024)
+    conn.close()
     if data:
         print(data.decode())
         if data.decode() == 'adminadmin':
             re_data = 'True'
+            conn, addr = sock.accept()
             conn.send(re_data.encode())
-        conn.close()
+            inf = conn.recv(1024).decode()
+            conn.close()
+            if inf == 'True':                 
+                clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                clientsocket.connect(('localhost', 7777))
+                while True:
+                    image = pyautogui.screenshot()
+                    image = image.resize((192, 108), Image.ANTIALIAS)
+                    image = np.array(image)
+                    img = Image.frombytes('RGB', (192, 108), image)
+                    data = pickle.dumps(np.array(img))
+                    clientsocket.sendall(struct.pack("L", len(data)) + data)
 
